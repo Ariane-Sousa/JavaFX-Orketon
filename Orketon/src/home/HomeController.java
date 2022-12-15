df2cc357e2de74d52ad142901a61a7b57061a760
Ariane-Sousa <ariane.crisousa@gmail.com>
2022-12-14 21:01:58 -0300

    Front tela Home Candidato

--- Orketon/src/home/HomeController.java
+++ Orketon/src/home/HomeController.java
@@ -1,13 +1,26 @@
 package home;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.layout.AnchorPane;
 
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class HomeController implements Initializable {
+
+    @FXML private AnchorPane dashboard;
+    @FXML private Button fechar;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        dashboard.toFront();
+    }
+
+    @FXML
+    private void fechar(ActionEvent event){
+        System.exit(0);
     }
 }
